7f92ea8229d0b0c969429b86e6a0e8d5ba7e2f10
Will Henderson <git@mail.jjbig.com>
2024-07-12 22:52:47 +1000

    remove old versions

--- src/lib/builders/chart2/types-util.ts
+++
@@ -1,4 +0,0 @@
-import type { Readable } from 'svelte/store';
-
-export type OptionalStore<TYPE> = TYPE | Readable<TYPE>;
-export type Map2OptionalStore<TYPE> = { [k in keyof TYPE] : k extends 'ordinal' ? TYPE[k] : OptionalStore<TYPE[k]> }
